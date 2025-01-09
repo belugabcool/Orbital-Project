4550e8f4468faab9f7ceb863385432af8b975468
Justin Lyu Kim <66839733+belugabcool@users.noreply.github.com>
2025-01-09 00:50:25 -0800

    Update satellite_orbit.py

--- Originals/satellite_orbit.py
+++ Originals/satellite_orbit.py
@@ -2,51 +2,50 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-# Constants
+#const/cond
 G = 6.67430e-11  # gravitational constant, m^3 kg^-1 s^-2
 M = 5.972e24  # mass of the Earth, kg
 R = 6.371e6  # radius of the Earth, m
 
-# Initial conditions
 r0 = R + 2000e3 # initial satellite distance from Earth's center, m (400 km altitude)
 # v0 = np.sqrt(G * M / r0) * 1.0  # initial speed for a near-circular orbit, m/s
 v0 =  8000  # initial speed for a near-circular orbit, m/s
 initial_state = [r0, 0, 0, v0]  # [x0, y0, vx0, vy0]
 frameScale = 7
 
-# Time parameters
+#time 
 t0 = 0
 orbit_period = 10 * 2 * np.pi * np.sqrt(r0**3 / (G * M)) 
 tf = orbit_period * 10
 n_steps = 3000
 dt = (tf - t0) / n_steps
 
-# Initialize arrays to store the solution
-t_values = np.linspace(t0, tf, n_steps + 1)
-x_values = np.zeros(n_steps + 1)
-y_values = np.zeros(n_steps + 1)
-vx_values = np.zeros(n_steps + 1)
-vy_values = np.zeros(n_steps + 1)
+
+t = np.linspace(t0, tf, n_steps + 1)
+x = np.zeros(n_steps + 1)
+y = np.zeros(n_steps + 1)
+vx = np.zeros(n_steps + 1)
+vy = np.zeros(n_steps + 1)
 
 # Set initial conditions
-x_values[0], y_values[0] = initial_state[0], initial_state[1]
-vx_values[0], vy_values[0] = initial_state[2], initial_state[3]
+x[0], y[0] = initial_state[0], initial_state[1]
+vx[0], vy[0] = initial_state[2], initial_state[3]
 
 # Velocity Verlet method to solve the equations of motion
 for i in range(n_steps):
-    r = np.sqrt(x_values[i]**2 + y_values[i]**2)
-    ax = -G * M * x_values[i] / r**3
-    ay = -G * M * y_values[i] / r**3
+    r = np.sqrt(x[i]**2 + y[i]**2)
+    ax = -G * M * x[i] / r**3
+    ay = -G * M * y[i] / r**3
     
-    x_values[i + 1] = x_values[i] + vx_values[i] * dt + 0.5 * ax * dt**2
-    y_values[i + 1] = y_values[i] + vy_values[i] * dt + 0.5 * ay * dt**2
+    x[i + 1] = x[i] + vx[i] * dt + 0.5 * ax * dt**2
+    y[i + 1] = y[i] + vy[i] * dt + 0.5 * ay * dt**2
     
-    r_new = np.sqrt(x_values[i + 1]**2 + y_values[i + 1]**2)
-    ax_new = -G * M * x_values[i + 1] / r_new**3
-    ay_new = -G * M * y_values[i + 1] / r_new**3
+    r_new = np.sqrt(x[i + 1]**2 + y[i + 1]**2)
+    ax_new = -G * M * x[i + 1] / r_new**3
+    ay_new = -G * M * y[i + 1] / r_new**3
     
-    vx_values[i + 1] = vx_values[i] + 0.5 * (ax + ax_new) * dt
-    vy_values[i + 1] = vy_values[i] + 0.5 * (ay + ay_new) * dt
+    vx[i + 1] = vx[i] + 0.5 * (ax + ax_new) * dt
+    vy[i + 1] = vy[i] + 0.5 * (ay + ay_new) * dt
 
 # Create the animation
 fig, ax = plt.subplots(figsize=(8, 8))
@@ -66,10 +65,10 @@ def init():
     return satellite, trail
 
 def update(frame):
-    satellite.set_data(x_values[frame], y_values[frame])
-    trail.set_data(x_values[:frame], y_values[:frame])
+    satellite.set_data(x[frame], y[frame])
+    trail.set_data(x[:frame], y[:frame])
     return satellite, trail
 
-ani = FuncAnimation(fig, update, frames=len(t_values), init_func=init, blit=True, interval=10)
+ani = FuncAnimation(fig, update, frames=len(t), init_func=init, blit=True, interval=10)
 
 plt.show()
