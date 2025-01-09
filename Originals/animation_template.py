f8eecb15bd958b040dba93540d3605b5220d5cb7
Justin Lyu Kim <66839733+belugabcool@users.noreply.github.com>
2025-01-09 00:52:42 -0800

    Update animation_template.py

--- Originals/animation_template.py
+++ Originals/animation_template.py
@@ -19,37 +19,37 @@ n_steps = 100
 dt = (tf - t0) / n_steps
 
 
-t_values = np.linspace(t0, tf, n_steps + 1)
-x_values = np.zeros(n_steps + 1)
-y_values = np.zeros(n_steps + 1)
-vx_values = np.zeros(n_steps + 1)
-vy_values = np.zeros(n_steps + 1)
+t = np.linspace(t0, tf, n_steps + 1)
+x = np.zeros(n_steps + 1)
+y = np.zeros(n_steps + 1)
+vx = np.zeros(n_steps + 1)
+vy = np.zeros(n_steps + 1)
 
 
-x_values[0], y_values[0] = x0, y0
-vx_values[0], vy_values[0] = vx0, vy0
+x[0], y[0] = x0, y0
+vx[0], vy[0] = vx0, vy0
 
 # euler method 
 for i in range(n_steps):
-    x_values[i + 1] = x_values[i] + dt * vx_values[i]
-    y_values[i + 1] = y_values[i] + dt * vy_values[i]
-    vx_values[i + 1] = vx_values[i]
-    vy_values[i + 1] = vy_values[i] #- dt * g
+    x[i + 1] = x[i] + dt * vx[i]
+    y[i + 1] = y[i] + dt * vy[i]
+    vx[i + 1] = vx[i]
+    vy[i + 1] = vy[i] #- dt * g
 
 
 fig, ax = plt.subplots()
-ax.set_xlim(0, max(x_values) + 5)
-ax.set_ylim(0, max(y_values) + 5)
-point, = ax.plot(x_values[0], y_values[0], 'bo')
+ax.set_xlim(0, max(x) + 5)
+ax.set_ylim(0, max(y) + 5)
+point, = ax.plot(x[0], y[0], 'bo')
 
 def init():
-    point.set_data(x_values[0], y_values[0])
+    point.set_data(x[0], y[0])
     return point,
 
 def update(frame):
-    point.set_data(x_values[frame], y_values[frame])
+    point.set_data(x[frame], y[frame])
     return point,
 
-ani = FuncAnimation(fig, update, frames=len(x_values), init_func=init, blit=True, interval=25)
+ani = FuncAnimation(fig, update, frames=len(x), init_func=init, blit=True, interval=25)
 
 plt.show()
